b31cd618252b0a6cac9edf36bb36de814d9c6fc6
Bohdan Ivanychko <>
2023-09-16 13:51:58 +0300

    Added global settings

--- src/main.ts
+++ src/main.ts
@@ -1,9 +1,38 @@
+import { ValidationPipe, VersioningType } from "@nestjs/common";
 import { NestFactory } from "@nestjs/core";
+import * as compression from "compression";
+import helmet from "helmet";
 
 import { AppModule } from "./app.module";
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors();
+
+  app.use(compression());
+  app.use(
+    helmet({
+      crossOriginEmbedderPolicy: { policy: "credentialless" },
+      crossOriginResourcePolicy: { policy: "cross-origin" },
+    }),
+  );
+
+  app.setGlobalPrefix("api");
+
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      transform: true,
+    }),
+  );
+
+  app.enableVersioning({
+    type: VersioningType.HEADER,
+    header: "Version",
+  });
+
   await app.listen(3000);
 }
+
 bootstrap();
